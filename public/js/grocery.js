27ba5922158af0a982c845a24777dba831f4a76a
Collin Wasley <cwasley@ucsd.edu>
2017-02-16 21:48:00 -0800

    Some changes made to grocery pages and js behind it

--- public/js/grocery.js
+++ public/js/grocery.js
@@ -11,9 +11,26 @@ $(document).ready(function() {
 function initializePage() {
 
   $.get("/grocerylist", getGroceryList);
+
+  $('.checkbox').click(moveGroceryItem);
+  // ADD click listener here
+  // DELETE click listener here
+
 }
 
 function getGroceryList(result) {
   console.log(result);
-  // $()
+  $.each( result['groceries'], function (index, value) {
+    console.log ("Ran");
+    var htmlToInject = '<div class="checkbox" id="' + index + '"> <label>' +
+    '<input type="checkbox" value="">' + 'Name: ' + value.name + ' Quantity: ' + value.quantity
+    + '</label></div>';
+    $('#groceryList').append(htmlToInject);
+  });
+}
+
+function moveGroceryItem(e) {
+  e.preventDefault();
+
+  console.log()
 }
--- routes/grocery.js
+++ routes/grocery.js
@@ -1,6 +1,12 @@
 var groceries = require('../grocery.json');
+// var inventory = require('../inventory.json'); TODO
 
 exports.groceryList = function(req, res) {
 
 	res.json(groceries);
 };
+
+exports.removeGroceryItem = function(req, res) {
+	//remove
+	//refresh page
+}
