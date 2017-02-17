50d8b35c49666bed05333622f17ea7a405c5ff31
Collin Wasley <cwasley@ucsd.edu>
2017-02-16 18:32:18 -0800

    Modified some of the html files and finished the grocery list json implementation

--- routes/grocery.js
+++ routes/grocery.js
@@ -1,6 +1,6 @@
 var groceries = require('../grocery.json');
 
 exports.groceryList = function(req, res) {
-	// get a random palette from the top ones
+
 	res.json(groceries);
-	}
+};
--- routes/ingredients.js
+++ routes/ingredients.js
@@ -3,4 +3,4 @@ var ingredients = require('../grocery.json');
 exports.ingredientsList = function(req, res) {
 	// get a random palette from the top ones
 	res.json(ingredients);
-	}
+};
