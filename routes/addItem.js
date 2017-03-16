49e2161f023e7099d8493ae42216c5d3a71b32ee
cpanlasigui <c.panlasigui@gmail.com>
2017-03-15 21:01:32 -0700

    Style changes and made font uniform

--- routes/addItem.js
+++ routes/addItem.js
@@ -1,5 +1,5 @@
 var list = require('../grocery.json');
-var inventoryList = require('../inventory.json');
+
 
 exports.addItem = function(req, res) {
 
@@ -13,13 +13,3 @@ exports.addItem = function(req, res) {
   );
   res.render('grocery');
 };
-
-exports.addInventoryItem = function(req, res) {
-
-  var newItem = {
-    name: req.query.name,
-    quantity: req.query.quantity,
-    units: req.query.units
-  };
-  inventoryList.ingredients.push(newItem);
-}
